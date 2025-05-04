25c5697c5bf25efacdeda44c3e08362ad3c58cb7
Miguel Corbacho <mikelcrash63@gmail.com>
2025-05-04 10:51:07 +0200

    Añadido aplicacion de firewall

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # deploy-context
 Repositorio para hacer inicializar y/migrar apps de manera rapida y automatizada. Basado en Ansible y Docker
+
+# Para hetzner hace falta tener instalado hcloud
+```bash
+brew install hcloud
+```
+
+# Necesario tener instalado tambien jq
+```bash
+brew install jq
+```
--- create-server.sh
+++ create-server.sh
@@ -7,6 +7,13 @@ IMAGE="alma-9"
 SSH_KEY_NAME="mch-portatil"
 LOCATION="fsn1" # Cambia esto si prefieres otra ubicación, como "nbg1" o "hel1"
 CLOUD_INIT_FILE="cloud-init.yml"
+FIREWALL_NAME="firewall-1"
+
+# Instalar jq si no está instalado
+if ! command -v jq &> /dev/null; then
+  echo "jq no está instalado. Instalando..."
+  apt-get install -y jq
+fi
 
 # Crear el servidor
 hcloud server create \
@@ -20,6 +27,18 @@ hcloud server create \
 # Verificar si el servidor fue creado correctamente
 if [ $? -eq 0 ]; then
   echo "El servidor '$SERVER_NAME' ha sido creado exitosamente."
+
+  # Obtener el ID del servidor recién creado
+  SERVER_ID=$(hcloud server list --selector "name=$SERVER_NAME" -o json | jq -r '.[0].id')
+
+  # Aplicar el firewall al servidor
+  hcloud firewall apply-to-resource "$FIREWALL_NAME" --type server --server "$SERVER_ID"
+  if [ $? -eq 0 ]; then
+    echo "El firewall '$FIREWALL_NAME' ha sido aplicado al servidor '$SERVER_NAME'."
+  else
+    echo "Error al aplicar el firewall '$FIREWALL_NAME' al servidor '$SERVER_NAME'."
+    exit 1
+  fi
 else
   echo "Error al crear el servidor '$SERVER_NAME'."
   exit 1
